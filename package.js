866526b8f11f754246c2bca283819ca986d82b0a
Rnhmjoj <micheleguerinirocco@me.com>
2014-06-29 02:27:19 +0200

    Improve meteor support

--- package.js
+++ package.js
@@ -1,3 +1,42 @@
 Package.describe({
-	summary: "Less-only Flat UI that doesn't require javascript",
+	summary: "Less-only Flat UI that doesn't require javascript"
+});
+
+Package.on_use(function (api){
+	api.use('less');
+	api.use('jquery');
+
+	var fonts_path = 'lib/fonts/lato/';
+	var bootstrap_path = 'lib/bootstrap/';
+
+	// Style and scripts
+	api.add_files(bootstrap_path + 'js/bootstrap.js'), 'client');
+	api.add_files(bootstrap_path + 'less/bootstrap.import.less'), 'client');
+	api.add_files('lib/less/flat-ui.import.less'), 'client');
+
+	// Fonts
+	api.add_files(fonts_path + 'lato-black.eot'), 'client');
+	api.add_files(fonts_path + 'lato-black.svg'), 'client');
+	api.add_files(fonts_path + 'lato-black.ttf'), 'client');
+	api.add_files(fonts_path + 'lato-black.woff'), 'client');
+	api.add_files(fonts_path + 'lato-bold.eot'), 'client');
+	api.add_files(fonts_path + 'lato-bold.svg'), 'client');
+	api.add_files(fonts_path + 'lato-bold.ttf'), 'client');
+	api.add_files(fonts_path + 'lato-bold.woff'), 'client');
+	api.add_files(fonts_path + 'lato-bolditalic.eot'), 'client');
+	api.add_files(fonts_path + 'lato-bolditalic.svg'), 'client');
+	api.add_files(fonts_path + 'lato-bolditalic.ttf'), 'client');
+	api.add_files(fonts_path + 'lato-bolditalic.woff'), 'client');
+	api.add_files(fonts_path + 'lato-italic.eot'), 'client');
+	api.add_files(fonts_path + 'lato-italic.svg'), 'client');
+	api.add_files(fonts_path + 'lato-italic.ttf'), 'client');
+	api.add_files(fonts_path + 'lato-italic.woff'), 'client');
+	api.add_files(fonts_path + 'lato-light.eot'), 'client');
+	api.add_files(fonts_path + 'lato-light.svg'), 'client');
+	api.add_files(fonts_path + 'lato-light.ttf'), 'client');
+	api.add_files(fonts_path + 'lato-light.woff'), 'client');
+	api.add_files(fonts_path + 'lato-regular.eot'), 'client');
+	api.add_files(fonts_path + 'lato-regular.svg'), 'client');
+	api.add_files(fonts_path + 'lato-regular.ttf'), 'client');
+	api.add_files(fonts_path + 'lato-regular.woff'), 'client');
 });
